0c15b452de1150775e99f13415c1cd2d6816474a
Alex <39405237+iamguruman@users.noreply.github.com>
2021-12-05 12:06:29 +0300

    Update create.php

--- views/upload/create.php
+++ views/upload/create.php
@@ -5,6 +5,8 @@ use yii\helpers\Html;
 /* @var $this yii\web\View */
 /* @var $model app\modules\project\models\MProjectUpload */
 
+$module = app\modules\project\Module:moduleId;
+
 $this->title = 'Добавить файл';
 
 $this->params['breadcrumbs'][] = \app\modules\project\Module::getBreadcrumbs();
@@ -13,7 +15,7 @@ if($model->object){
     $this->params['breadcrumbs'][] = $model->object->getBreadcrumbs();
 
     $this->params['breadcrumbs'][] = ['label' => 'Файлы',
-        'url' => ['/project/default/view', 'id' => $model->object->id, 'tab' => 'files']];
+        'url' => ["/{$module}/default/view", 'id' => $model->object->id, 'tab' => 'files']];
 }
 
 $this->params['breadcrumbs'][] = $this->title;
