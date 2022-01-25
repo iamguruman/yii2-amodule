3735b6616bbce5761258d157c3cfe1c79a37c379
Alex <39405237+iamguruman@users.noreply.github.com>
2022-01-25 11:50:48 +0300

    Update update.php

--- views/upload/update.php
+++ views/upload/update.php
@@ -3,19 +3,18 @@
 use yii\helpers\Html;
 
 /* @var $this yii\web\View */
-
-/* @var $model app\modules\project\models\MProjectUpload */
+/* @var $model app\modules\{_MODULE_ID_}\models\{_UPLOAD_MODEL_NAME_} */
 
 $this->title = $model->filename_original;
 
-$this->params['breadcrumbs'][] = \app\modules\project\Module::getBreadcrumbs();
+$this->params['breadcrumbs'][] = \app\modules\{_MODULE_ID_}\Module::getBreadcrumbs();
 
 $this->params['breadcrumbs'][] = $model->object->getBreadcrumbs();
 
 $this->params['breadcrumbs'][] = 'Редактирование';
 
 ?>
-<div class="mproject-upload-update">
+<div class="m{_MODULE_ID_}-upload-update">
 
     <h1><?= Html::encode($this->title) ?></h1>
 
