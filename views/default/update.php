e8a0dae6615d6128f187acf64010cb5c2f5f9e42
Alex <39405237+iamguruman@users.noreply.github.com>
2022-01-25 11:41:33 +0300

    Update update.php

--- views/default/update.php
+++ views/default/update.php
@@ -3,18 +3,18 @@
 use yii\helpers\Html;
 
 /* @var $this yii\web\View */
-/* @var $model app\modules\project\models\MProject */
+/* @var $model app\modules\{_MODULE_ID_}\models\{_OBJECT_MODEL_NAME_} */
 
 $this->title = $model->getTitle();
 
-$this->params['breadcrumbs'][] = \app\modules\project\Module::getBreadcrumbs();
+$this->params['breadcrumbs'][] = \app\modules\{_MODULE_ID_}\Module::getBreadcrumbs();
 
 $this->params['breadcrumbs'][] = $model->getBreadcrumbs();
 
 $this->params['breadcrumbs'][] = 'Редактирование';
 
 ?>
-<div class="mproject-update">
+<div class="m{_MODULE_ID_}-update">
 
     <h1><?= Html::encode($this->title) ?></h1>
 
