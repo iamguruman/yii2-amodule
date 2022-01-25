9b3331fa2ecb2391c2ae9871291753cea5ad3d05
Alex <39405237+iamguruman@users.noreply.github.com>
2022-01-25 11:43:48 +0300

    Update _form.php

--- views/upload/_form.php
+++ views/upload/_form.php
@@ -4,22 +4,21 @@ use yii\helpers\Html;
 use yii\widgets\ActiveForm;
 
 /* @var $this yii\web\View */
-/* @var $model app\modules\project\models\MProjectUpload */
+/* @var $model app\modules\{_MODULE_ID_}\models\{_UPLOAD_MODEL_NAME_} */
 /* @var $form yii\widgets\ActiveForm */
 
-$module =app\modules\project\Module::moduleId;
+$module = app\modules\{_MODULE_ID_}\Module::moduleId;
 
 ?>
 
-<div class="mproject-upload-form">
+<div class="m{_MODULE_ID_}-upload-form">
 
     <?php $form = ActiveForm::begin(); ?>
 
     <?= (aIfModuleControllerAction($module, "upload", "view") &&
         aUserMyId() == 1) ? $form->field($model, 'kp_id')->textInput() : null ?>
 
-    <?= (aIfModuleControllerAction($module, "upload", "update") &&
-        aUserMyId() == 1) ? $form->field($model, 'filename_original')->textInput(['maxlength' => true]) : null ?>
+    <?= (aIfModuleControllerAction($module, "upload", "update")) ? $form->field($model, 'filename_original')->textInput(['maxlength' => true]) : null ?>
 
     <?= aIfModuleControllerAction($module, "upload", "create") ?
         $form->field($model, 'files[]')
