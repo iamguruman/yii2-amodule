4aa5b7a37da2fdb1b10de9f481c64af18c8cd841
Alex <39405237+iamguruman@users.noreply.github.com>
2022-01-27 21:03:24 +0300

    Update _form.php

--- views/default/_form.php
+++ views/default/_form.php
@@ -4,7 +4,7 @@ use yii\helpers\Html;
 use yii\widgets\ActiveForm;
 
 /* @var $this yii\web\View */
-/* @var $model app\modules\project\models\MProject */
+/* @var $model app\modules\{_MODULE_ID_}\models\{_OBJECT_MODEL_NAME_} */
 /* @var $form yii\widgets\ActiveForm */
 ?>
 
