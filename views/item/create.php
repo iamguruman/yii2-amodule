98a0f858ea6254b441f833700732fc56f4771d80
Alex <39405237+iamguruman@users.noreply.github.com>
2024-09-01 22:41:44 +0300

    Create create.php

---
+++ views/item/create.php
@@ -0,0 +1,27 @@
+<?php
+
+use yii\helpers\Html;
+
+/* @var $this yii\web\View */
+/* @var $model app\modules\{_MODULE_ID_}\models\{_OBJECT_MODEL_NAME_}Item */
+
+$this->title = 'Добавить';
+
+$this->params['breadcrumbs'][] = app\modules\{_MODULE_ID_}\Module::getBreadcrumbs();
+
+if($model->parentObject){
+    $this->params['breadcrumbs'][] = $model->parentObject->getBreadcrumbs();
+}
+
+$this->params['breadcrumbs'][] = $this->title;
+
+?>
+<div class="{_MODULE_ID_}-item-create">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <?= $this->render('_form', [
+        'model' => $model,
+    ]) ?>
+
+</div>
