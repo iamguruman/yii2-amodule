6673edc56fd96326566c8d5b7072d1911e6303db
Alex <39405237+iamguruman@users.noreply.github.com>
2024-09-01 22:51:20 +0300

    Update _search.php

--- views/item/_search.php
+++ views/item/_search.php
@@ -4,7 +4,7 @@ use yii\helpers\Html;
 use yii\widgets\ActiveForm;
 
 /* @var $this yii\web\View */
-/* @var $model app\modules\{_MODULE_ID_}\models\{_OBJECT_MODEL_NAME_}ItemUploadSearch */
+/* @var $model app\modules\{_MODULE_ID_}\models\{_OBJECT_MODEL_NAME_}ItemSearch */
 /* @var $form yii\widgets\ActiveForm */
 ?>
 
