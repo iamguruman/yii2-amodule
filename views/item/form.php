a8ad4ff7b00ddc879c6e71d8f09fbe4592d0153e
Alex <39405237+iamguruman@users.noreply.github.com>
2024-09-01 22:38:30 +0300

    Create form.php

---
+++ views/item/form.php
@@ -0,0 +1,25 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model app\modules\{_MODULE_ID_}\models\{_OBJECT_MODEL_NAME_}Item */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="{_MODULE_ID_}-item-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, '{_ITEM_TABLE_PARENT_ID_FIELD_}')->textInput() ?>
+
+    <?= $form->field($model, 'name')->textInput() ?>
+
+    <div class="form-group">
+        <?= Html::submitButton('Сохранить', ['class' => 'btn btn-success']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
