923bc3dfa13ed3a456fd930b667235492f6869a7
Alex <39405237+iamguruman@users.noreply.github.com>
2024-09-01 22:46:01 +0300

    Create update.php

---
+++ views/item/update.php
@@ -0,0 +1,32 @@
+<?php
+
+use yii\helpers\Html;
+
+/* @var $this yii\web\View */
+/* @var $model app\modules\{_MODULE_ID_}\models\{_OBJECT_MODEL_NAME_}Item */
+
+$this->title = $model->getTitle();
+
+$this->params['breadcrumbs'][] = \app\modules\{_MODULE_ID_}\Module::getBreadcrumbs();
+
+$this->params['breadcrumbs'][] = $model->getBreadcrumbs();
+
+$this->params['breadcrumbs'][] = 'Редактирование';
+
+?>
+<div class="{_MODULE_ID_}-item-update">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>
+
+        <?= aHtmlButtonDelete($model) ?>
+
+    </p>
+
+
+    <?= $this->render('_form', [
+        'model' => $model,
+    ]) ?>
+
+</div>
