d480e6ba28825d8d58573c252c9481dce2a0343d
Alex <39405237+iamguruman@users.noreply.github.com>
2024-11-21 15:57:36 +0300

    Update _form.php

--- views/item-upload/_form.php
+++ views/item-upload/_form.php
@@ -4,22 +4,22 @@ use yii\helpers\Html;
 use yii\widgets\ActiveForm;
 
 /* @var $this yii\web\View */
-/* @var $model app\modules\{_MODULE_ID_}\models\{_OBJECT_MODEL_NAME_}ItemUpload */
+/* @var $model app\modules\{_MODULE_ID_}\models\{_OBJECT_MODEL_NAME_}{ITEM_NAME}Upload */
 /* @var $form yii\widgets\ActiveForm */
 
 $module = app\modules\{_MODULE_ID_}\Module::moduleId;
 
 ?>
 
-<div class="{_MODULE_ID_}-item-upload-form">
+<div class="{_MODULE_ID_}-{ITEM_NAME_LOWCASE}-upload-form">
 
     <?php $form = ActiveForm::begin(); ?>
 
-    <?= aIfModuleControllerAction($module, "item-upload", "update") ? $form->field($model, 'object_id')->textInput() : null ?>
+    <?= aIfModuleControllerAction($module, "{ITEM_NAME_LOWCASE}-upload", "update") ? $form->field($model, 'object_id')->textInput() : null ?>
 
-    <?= (aIfModuleControllerAction($module, "item-upload", "update")) ? $form->field($model, 'filename_original')->textInput(['maxlength' => true]) : null ?>
+    <?= (aIfModuleControllerAction($module, "{ITEM_NAME_LOWCASE}-upload", "update")) ? $form->field($model, 'filename_original')->textInput(['maxlength' => true]) : null ?>
 
-    <?= aIfModuleControllerAction($module, "item-upload", "create") ?
+    <?= aIfModuleControllerAction($module, "ITEM_NAME_LOWCASE-upload", "create") ?
         $form->field($model, 'files[]')
             ->fileInput(['multiple' => true])
         //->fileInput(['multiple' => true])
