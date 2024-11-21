28ff78ec7d4e3e013c1732b03bfbdc1a7c29a125
Alex <39405237+iamguruman@users.noreply.github.com>
2024-11-21 17:10:39 +0300

    Update update.php

--- views/item-upload/update.php
+++ views/item-upload/update.php
@@ -3,7 +3,7 @@
 use yii\helpers\Html;
 
 /* @var $this yii\web\View */
-/* @var $model app\modules\{_MODULE_ID_}\models\{_OBJECT_MODEL_NAME_}ItemUpload */
+/* @var $model app\modules\{_MODULE_ID_}\models\{_OBJECT_MODEL_NAME_}{ITEM_NAME}Upload */
 
 $this->title = $model->filename_original;
 
@@ -14,7 +14,7 @@ $this->params['breadcrumbs'][] = $model->object->getBreadcrumbs();
 $this->params['breadcrumbs'][] = 'Редактирование';
 
 ?>
-<div class="_MODULE_ID_-item-upload-update">
+<div class="_MODULE_ID_-{ITEM_NAME_LOWCASE}-upload-update">
 
     <h1><?= Html::encode($this->title) ?></h1>
 
