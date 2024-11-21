47b2461fe2e5fb45db8b41e6c6733f4553d0fad9
Alex <39405237+iamguruman@users.noreply.github.com>
2024-11-21 17:22:10 +0300

    Update update.php

--- views/item/update.php
+++ views/item/update.php
@@ -3,7 +3,7 @@
 use yii\helpers\Html;
 
 /* @var $this yii\web\View */
-/* @var $model app\modules\{_MODULE_ID_}\models\{_OBJECT_MODEL_NAME_}Item */
+/* @var $model app\modules\{_MODULE_ID_}\models\{_OBJECT_MODEL_NAME_}{ITEM_NAME} */
 
 $this->title = $model->getTitle();
 
@@ -14,7 +14,7 @@ $this->params['breadcrumbs'][] = $model->getBreadcrumbs();
 $this->params['breadcrumbs'][] = 'Редактирование';
 
 ?>
-<div class="{_MODULE_ID_}-item-update">
+<div class="{_MODULE_ID_}-{ITEM_NAME_LOWCASE}-update">
 
     <h1><?= Html::encode($this->title) ?></h1>
 
