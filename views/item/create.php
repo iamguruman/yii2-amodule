43515b0b4b2f5b816a6c3223ab109295c2c5ff55
Alex <39405237+iamguruman@users.noreply.github.com>
2024-11-21 17:18:17 +0300

    Update create.php

--- views/item/create.php
+++ views/item/create.php
@@ -3,7 +3,7 @@
 use yii\helpers\Html;
 
 /* @var $this yii\web\View */
-/* @var $model app\modules\{_MODULE_ID_}\models\{_OBJECT_MODEL_NAME_}Item */
+/* @var $model app\modules\{_MODULE_ID_}\models\{_OBJECT_MODEL_NAME_}{ITEM_NAME} */
 
 $this->title = 'Добавить';
 
@@ -16,7 +16,7 @@ if($model->parentObject){
 $this->params['breadcrumbs'][] = $this->title;
 
 ?>
-<div class="{_MODULE_ID_}-item-create">
+<div class="{_MODULE_ID_}-{ITEM_NAME_LOWCASE}-create">
 
     <h1><?= Html::encode($this->title) ?></h1>
 
